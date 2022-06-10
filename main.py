3ff2c37ea64db2a8e1029ffec71333ae0fe7114b
loovjo <jonathan.loov@gmail.com>
2022-06-10 23:03:27 +0200

    !bing & !bong

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import logging
 
 PING_CHANNEL_ID = 984834553782886480
 SVX_ROLE = 984735416341114910
+PING_ROLE = 984735416341114910
 
 BOT_LOG = logging.getLogger("bot")
 
@@ -21,6 +22,39 @@ class SvxNotifierBot(discord.Client):
     async def on_ready(self):
         BOT_LOG.info("Bot ready!")
         self.bot_ch = await self.fetch_channel(PING_CHANNEL_ID)
+
+    async def on_message(self, msg: discord.Message):
+        author = msg.author
+        if not isinstance(author, discord.Member):
+            return
+
+        guild = author.guild
+        ping_role = guild.get_role(PING_ROLE)
+
+        if ping_role is None:
+            BOT_LOG.critical("Ping role not found!")
+            return
+
+        # add role
+        if msg.content == "!bing":
+            # BOT_LOG.
+            if not ping_role in author.roles:
+                BOT_LOG.info(f"Giving {author.name} ping role")
+                await author.add_roles(ping_role, reason="!bing")
+                await msg.reply(f"You have been given the *@{ping_role.name}* role")
+            else:
+                BOT_LOG.info(f"{author.name} already has ping role")
+                await msg.reply(f"You already have the *@{ping_role.name}* role. Use !bong to remove it")
+
+        # remove role
+        if msg.content == "!bong":
+            if ping_role in author.roles:
+                BOT_LOG.info(f"Removing {author.name} ping role")
+                await author.remove_roles(ping_role, reason="!bong")
+                await msg.reply(f"You no longer have the *@{ping_role.name}* role")
+            else:
+                await msg.reply(f"You don't have the *@{ping_role.name}* role. Use !bing to acquire it")
+
 
     async def svx_notification(self, node: svx.Node, time_since_last: Optional[float]):
         if self.bot_ch is None:
