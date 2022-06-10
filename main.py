11dabec1399c234b31604195049acb06bbdc863d
loovjo <jonathan.loov@gmail.com>
2022-06-10 23:06:05 +0200

    fel language

--- main.py
+++ main.py
@@ -41,19 +41,19 @@ class SvxNotifierBot(discord.Client):
             if not ping_role in author.roles:
                 BOT_LOG.info(f"Giving {author.name} ping role")
                 await author.add_roles(ping_role, reason="!bing")
-                await msg.reply(f"You have been given the *@{ping_role.name}* role")
+                await msg.reply(f"Du har nu fått rollen **@{ping_role.name}**")
             else:
                 BOT_LOG.info(f"{author.name} already has ping role")
-                await msg.reply(f"You already have the *@{ping_role.name}* role. Use !bong to remove it")
+                await msg.reply(f"Du har redan rollen **@{ping_role.name}**. Använd !bong för att bli av med den!")
 
         # remove role
         if msg.content == "!bong":
             if ping_role in author.roles:
                 BOT_LOG.info(f"Removing {author.name} ping role")
                 await author.remove_roles(ping_role, reason="!bong")
-                await msg.reply(f"You no longer have the *@{ping_role.name}* role")
+                await msg.reply(f"Du har nu inte längre rollen **@{ping_role.name}**.")
             else:
-                await msg.reply(f"You don't have the *@{ping_role.name}* role. Use !bing to acquire it")
+                await msg.reply(f"Du har ej rollen **@{ping_role.name}**. Använd !bing för att få den!")
 
 
     async def svx_notification(self, node: svx.Node, time_since_last: Optional[float]):
